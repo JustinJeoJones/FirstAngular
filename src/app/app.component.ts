e2e4024c189d68934335b7f9c9767d3cf53212e9
Justin <Justin@grandcircus.co>
2022-09-28 21:11:19 -0400

    first

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,8 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'myapp';
+  names: string[] = ["Fred", "Julie", "Sam", "Olivia", "Adam", "Jennifer"];
+  isHungry:boolean = true;
+
+  currentDay:Date = new Date;
 }
